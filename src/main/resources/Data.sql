28c5564fff6779994b994798552037b0aea45709
ahmedhsultan <ahmedhussamsultan@gmail.com>
2023-07-07 17:13:23 +0300

    Data Initializer

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_table (id, name, age) VALUES (1, 'ahmed', 28);
+INSERT INTO user_table (id, name, age) VALUES (2, 'ali', 28);
+INSERT INTO user_table (id, name, age) VALUES (3, 'hana', 28);
+INSERT INTO user_table (id, name, age) VALUES (4, 'esraa', 28);
