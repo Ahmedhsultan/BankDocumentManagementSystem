531480d3be2fb5f79e5d317660ccb415c6a349cd
ahmedhsultan <ahmedhussamsultan@gmail.com>
2023-07-07 06:36:21 +0300

    initializr

---
+++ src/main/java/com/example/BankDocumentManagementSystem/BankDocumentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankDocumentManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDocumentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDocumentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BankDocumentManagementSystem/BankDocumentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankDocumentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDocumentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
