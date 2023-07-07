abe1925d9a896f3e03c4e680348d8c524ed2df2e
ahmedhsultan <ahmedhussamsultan@gmail.com>
2023-07-07 17:14:23 +0300

    Optimize imports

--- src/main/java/com/example/BankDocumentManagementSystem/controller/PostController.java
+++ src/main/java/com/example/BankDocumentManagementSystem/controller/PostController.java
@@ -1,13 +1,9 @@
 package com.example.BankDocumentManagementSystem.controller;
 
-import com.example.BankDocumentManagementSystem.dto.CommentDTO;
 import com.example.BankDocumentManagementSystem.dto.PostDTO;
 import com.example.BankDocumentManagementSystem.persistence.entity.Post;
-import com.example.BankDocumentManagementSystem.persistence.repository.CommentRepo;
 import com.example.BankDocumentManagementSystem.persistence.repository.PostRepo;
-import com.example.BankDocumentManagementSystem.service.CommentService;
 import com.example.BankDocumentManagementSystem.service.PostService;
-import com.example.BankDocumentManagementSystem.util.mapper.CommentMapper;
 import com.example.BankDocumentManagementSystem.util.mapper.PostMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
