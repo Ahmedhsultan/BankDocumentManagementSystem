5f13800b93addea14fcb26a8c20dc9738e2c87da
ahmedhsultan <ahmedhussamsultan@gmail.com>
2023-07-11 13:32:28 +0300

    post and comments

--- src/main/java/com/example/BankDocumentManagementSystem/BankDocumentManagementSystemApplication.java
+++ src/main/java/com/example/BankDocumentManagementSystem/BankDocumentManagementSystemApplication.java
@@ -2,10 +2,9 @@ package com.example.BankDocumentManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 public class BankDocumentManagementSystemApplication {
 
 	public static void main(String[] args) {
